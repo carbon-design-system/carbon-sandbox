030fd26f2486593b3bb87d018deb4a1da6a410c2
chrisdhanaraj <adhanaraj@gmail.com>
2017-05-25 09:55:39 -0500

    feat(api): build sass files

--- client/src/Components/Variable.js
+++ client/src/Components/Variable.js
@@ -5,7 +5,14 @@ export default class Variable extends Component {
     const { name, hex } = this.props;
     return (
       <li className="variable">
-        <input className="variable__input" type="color" defaultValue={hex} />
+        <input
+          className="variable__input"
+          onChange={evt => {
+            this.props.updateColor(name, evt.target.value);
+          }}
+          type="color"
+          value={hex}
+        />
         <p className="variable__name">{name}</p>
         <p className="variable__hex">{hex}</p>
       </li>
